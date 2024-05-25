90387f83b444f2fcde1201956237931ac450b3dd
Krushna Prasad Sahoo <64131038+Krushna-Prasad-Sahoo@users.noreply.github.com>
2024-05-25 22:42:20 +0530

    Added KCS function to check_ingress_cluster_operator.sh

--- scripts/check_ingress_cluster_operator.sh
+++ scripts/check_ingress_cluster_operator.sh
@@ -15,20 +15,10 @@ RESET="\033[0m"
 cluster_id=$1
 
 # Initialize a search string for Knowledge Center Search
-search_string="ingress%20operator%20in%20OpenShift"
+search_string="ingress+controller+%2B+operator+%2B+openshift"
 
 # Set a flag to indicate whether to perform Knowledge Center Search
 do_kcs_search="true"
-
-# Prompt the user to enter their username
-# Read the input from the user and store it in the variable 'username'
-echo -n "Enter your username (ex: rhn-support-<kerberos>): "
-read username
-
-# Prompt the user to enter their password (with the '-s' flag to silence input)
-# Read the input from the user without echoing it to the terminal (for password input)
-echo -n "Enter your password: "
-read -s pass
 
 # Echo an empty line for visual separation or formatting purposes
 echo
@@ -55,6 +45,17 @@ login_via_backplane() {
     # Echo a message indicating the start of the login process in yellow color
     echo -e "${YELLOW}Logging into the cluster via backplane...${RESET}"
     echo
+
+    # Prompt the user to enter their username
+    # Read the input from the user and store it in the variable 'username'
+    echo -n "Enter your username (ex: rhn-support-<kerberos>): "
+    read username
+
+    # Prompt the user to enter their password (with the '-s' flag to silence input)
+    # Read the input from the user without echoing it to the terminal (for password input)
+    echo -n "Enter your password: "
+    read -s pass
+
 
     # Use 'ocm backplane login' command to log into the cluster using the provided 'cluster_id'
     #ocm backplane login $cluster_id
@@ -228,7 +229,7 @@ check_ingress_cluster_operator_pod_logs() {
     oi_pod=$(oc -n openshift-ingress get pods --no-headers -o custom-columns=":metadata.name")
 
     # Define red flags indicating potential issues in logs
-    red_flags=("issue" "error" "degraded" "timeout" "expire" "not responding" "overload" "canceled" "RequestError" "Unavailable" "backoff" "failed" "unreachable" "x509" "connection error" "reconciliation failed" "not created" "conflict" "bottleneck" "congestion" "drop" "spike" "imbalance" "misconfiguration")
+    red_flags=("issue" "error" "degrade" "timeout" "expire" "not responding" "overload" "canceled" "RequestError" "Unavailable" "backoff" "failed" "unreachable" "x509" "connection error" "reconciliation failed" "not created" "conflict" "bottleneck" "congestion" "drop" "spike" "imbalance" "misconfiguration")
 
     # Check if the pods exist
     if [ -n "$oi_pod" ]; then
@@ -237,7 +238,7 @@ check_ingress_cluster_operator_pod_logs() {
         echo
 
         # Get the last 10 lines of logs from the operator pod and filter them for red flags
-        log_output=$(oc --tail 10 logs -n openshift-ingress "$oi_pod" | grep -iE 'issue|error|degraded|timeout|expire|not responding|overload|canceled|RequestError|Unavailable|backoff|failed|unreachable|x509|connection error|reconciliation failed|not created|conflict|bottleneck|congestion|drop|spike|imbalance|misconfiguration')
+        log_output=$(oc --tail 10 logs -n openshift-ingress "$oi_pod" | grep -iE 'issue|error|degrade|timeout|expire|not responding|overload|canceled|RequestError|Unavailable|backoff|failed|unreachable|x509|connection error|reconciliation failed|not created|conflict|bottleneck|congestion|drop|spike|imbalance|misconfiguration')
 
         # Colorize the logs containing red flags
         colored_logs="$log_output"
@@ -268,7 +269,7 @@ check_ingress_cluster_operator_pod_logs() {
         echo
 
         # Get the last 10 lines of logs from the operator pod and filter them for red flags
-        log_output=$(oc --tail 10 logs -n openshift-ingress "$oio_pod" | grep -iE 'issue|error|degraded|timeout|expire|not responding|overload|canceled|RequestError|Unavailable|backoff|failed|unreachable|x509|connection error|reconciliation failed|not created|conflict|bottleneck|congestion|drop|spike|imbalance|misconfiguration')
+        log_output=$(oc --tail 10 logs -n openshift-ingress-operator "$oio_pod" | grep -iE 'issue|error|degrade|timeout|expire|not responding|overload|canceled|RequestError|Unavailable|backoff|failed|unreachable|x509|connection error|reconciliation failed|not created|conflict|bottleneck|congestion|drop|spike|imbalance|misconfiguration')
 
         # Colorize the logs containing red flags
         colored_logs="$log_output"
@@ -292,9 +293,6 @@ check_ingress_cluster_operator_pod_logs() {
 check_ingress_controller_status() {
     # Echo a blank line for spacing
     echo
-    # Echo a separator line in green color for visual distinction
-    #echo -e "${GREEN}------------------------------------------------------------------------${RESET}"
-    #echo
 
     ics=$(oc get ingresscontroller -n openshift-ingress-operator --no-headers -o custom-columns=":metadata.name")
 
@@ -327,14 +325,85 @@ check_ingress_controller_status() {
 
 }
 
-# Function to build search string for KCS based on operator degraded message
-build_kcs_search_string() {
-    echo "I am build_kcs_search_string()"
-}
+# Function to build search string and find KCS based on operator degraded message
 
-# Function to search for KCS solutions based on the generated search string
-search_kcs() {
-    echo "I am searching KCS()"
+get_kcs() {
+    echo -e "${GREEN}------------------------------------------------------------------------${RESET}"
+    operator_degraded_message=$(oc get co ingress -o json | jq -r '.status.conditions[] | select(.type == "Degraded" and .status == "True") | .message')
+    if [ -z "$operator_degraded_message" ]; then
+        operator_degraded_message=$(oc get co ingress -o json | jq -r '.status.conditions[] | select(.type == "Progressing" and .status == "True") | .message')
+    fi
+
+    #echo -e "OPERTOR MESSAGE : $operator_degraded_message"
+
+    if [ -z "$operator_degraded_message" ]; then
+        do_kcs_search="false"
+    else
+        # Strings to search for
+        search_pattern=("IngressControllerUnavailable" "issue" "error" "degrade" "timeout" "expire" "not responding" "overload" "canceled" "RequestError" "Unavailable" "backoff" "failed" "unreachable" "x509" "connection error" "reconciliation failed" "not created" "conflict" "misconfiguration")
+
+        # Variable to store the found strings
+        found_strings=""
+
+        # Loop through each search string
+        for search_str in "${search_pattern[@]}"; do
+            # Check if the search string is present in the paragraph
+            if [[ $operator_degraded_message =~ $search_str ]]; then
+                # If found, append it to the variable
+                found_strings="$found_strings $search_str"
+            fi
+        done
+
+
+        updated_operator_degraded_message=$(echo "$found_strings" | sed 's/ /%20/g')
+        updated_search_string="$search_string$updated_operator_degraded_message"
+
+    fi
+
+    echo
+
+    disclaimer="The articles or solutions retrieved via this service may not be entirely suitable for your specific concerns. We advise verifying the content manually before considering or implementing any suggestions provided therein. While we strive to provide accurate information, the responsibility for validation ultimately rests with the user. Thank you for your understanding."
+
+    if [ "$do_kcs_search" == "false" ]; then
+        echo -e "${GREEN}We couldn't build a valid search string. It looks like the operator is not being reported as degraded. If there are issues with the operator, please review the logs and resources manually."
+
+        # page1=https://access.redhat.com/solutions?title=ingress&product=91541&category=All&state=published&kcs_state=All&language=en&field_internal_tags_tid=All
+
+        # page2=https://access.redhat.com/articles?title=ingress&kcs_article_type=All&product=91541&category=All&language=en&order=state&sort=desc
+
+    else
+        echo -e "${YELLOW}Searching for KCS Solutions...${RESET}"
+
+        api_url_primary="https://api.access.redhat.com/support/search/kcs?fq=documentKind:(%22Solution%22)&q=*$updated_search_string*&rows=3&start=0"
+
+        api_url_secondary="https://api.access.redhat.com/support/search/kcs?fq=documentKind:(%22Solution%22)&q=*$search_string*&rows=3&start=0"
+
+	    # Make the API call and store the response in a variable
+
+        KCS=$(curl -s -X GET -u "$username:$pass" "$api_url_primary" | grep -o 'https://access.redhat.com/solutions/[^ ]*' | sed -e 's/["}].*//')
+
+        if [ -z "$KCS" ]; then
+            KCS=$(curl -s -X GET -u "$username:$pass" "$api_url_secondary" | grep -o 'https://access.redhat.com/solutions/[^ ]*' | sed -e 's/["}].*//')
+        else
+            # KCS is not empty
+            # good to proceed
+            echo
+        fi
+
+        # Check if the API call was successful (HTTP status code 200)
+        http_status_codep=$(curl -s -o /dev/null -w "%{http_code}" "$api_url_primary")
+        http_status_codes=$(curl -s -o /dev/null -w "%{http_code}" "$api_url_secondary")
+
+        if [ "$http_status_codep" -eq 200 ] || [ "$http_status_codes" -eq 200 ] ; then
+            echo "Heya! we found some KCS for you : "
+            echo $KCS | sed 's/ /\n/g'
+            echo
+            echo $disclaimer
+            echo
+        else
+            echo "We apologise, the API request for 'KCS Search' has been failed with HTTP status code $http_status_code."
+        fi
+    fi
 }
 
 # Function to generate and display Prometheus graph links related to ingress operator metrics
@@ -345,20 +414,20 @@ get_prometheus_graph_links() {
 # Main function
 main() {
 
-    os_default_browser
-    login_via_backplane
-    get_basic_info
-    check_ingress_cluster_operator_status
+    #os_default_browser
+    #login_via_backplane
+    #get_basic_info
+    #check_ingress_cluster_operator_status
 
     # Check operator resources, events, operator pod logs and other configurations
-    check_ingress_cluster_operator_resources
-    check_ingress_cluster_operator_events
-    check_ingress_cluster_operator_pod_logs
-    check_ingress_controller_status
+    #check_ingress_cluster_operator_resources
+    #check_ingress_cluster_operator_events
+    #check_ingress_cluster_operator_pod_logs
+    #check_ingress_controller_status
 
     # Build KCS search string, search for KCS solutions, get Prometheus graph links
-    build_kcs_search_string
-    search_kcs
+    get_kcs
+    #search_kcs
     get_prometheus_graph_links
 
 }
